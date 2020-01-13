da0d2b3ecb71756df20dbbf7b76464e46a333fe2
Joseph Procopio <josephp27@users.noreply.github.com>
2020-01-13 10:00:29 -0600

    Enable recursive globbing

--- crypter.py
+++ crypter.py
@@ -1,7 +1,7 @@
 import os
 from cryptography.fernet import Fernet
 from glob import glob
-from find_files import find_all_files, load_git_crypt
+from find_files import load_git_crypt
 
 key = os.environ['ENCRYPTION_TOOLS_KEY']
 
@@ -27,8 +27,5 @@ def encrypt(path):
 specifiedFiles = load_git_crypt()
 
 for file in specifiedFiles:
-	for location in glob(file):
-		if os.path.isdir(location):
-			find_all_files(location, encrypt)
-		else:
-			encrypt(location)
+    for location in glob(file, recursive=True):
+        encrypt(location)
