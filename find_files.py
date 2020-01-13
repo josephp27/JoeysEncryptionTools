3f192081c1fc2b663f3de9271da083f3396b9446
Joseph Procopio <josephp27@users.noreply.github.com>
2020-01-13 10:03:21 -0600

    Remove find_all_files method

--- find_files.py
+++ find_files.py
@@ -1,23 +1,12 @@
 import os
 
-def find_all_files(path, callback):
-
-	for type_ in os.listdir(path):
-
-		full_path = path + '/' + type_
-
-		if os.path.isdir(full_path):
-			find_all_files(full_path, callback)
-		else:
-			callback(full_path)
-
 def load_git_crypt():
-	try:
-		file = open('.gitCrypt', 'r')
-	except IOError:
-		print('Cannot find .gitCrypt file. Aborting')
-		exit(1)
+    try:
+        file = open('.gitCrypt', 'r')
+    except IOError:
+        print('Cannot find .gitCrypt file. Aborting')
+        exit(1)
 
-	with open('.gitCrypt') as file:
-		lines = file.readlines()
-		return  [line.strip() for line in lines]
+    with open('.gitCrypt') as file:
+        lines = file.readlines()
+        return  [line.strip() for line in lines]
